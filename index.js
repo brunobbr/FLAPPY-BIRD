f36f55c796aa31189864bc11ecd400392e30f571
brunobbr <bruno-loll@hotmail.com>
2021-03-18 16:18:59 -0400

    part 3 animate

--- index.js
+++ index.js
@@ -35,12 +35,44 @@ function pairHurdling(heightH, opening, x){
         this.les.setHeightH(heightLes)
     }
     this.getX = () => parseInt(this.element.style.left.split('px')[0])
-    this.setX = () => this.element.style.left = `${x}px`
+    this.setX = x => this.element.style.left = `${x}px`
     this.getBreadth = () => this.element.clientWidth
 
     this.raffleOpening()
     this.setX(x)
 }
 
-const b = new pairHurdling(700, 200, 800)
-document.querySelector('[wm-flappy]').appendChild(b.element)
+//const b = new pairHurdling(700, 200, 800)
+//document.querySelector('[wm-flappy]').appendChild(b.element)
+
+function hurdlings(heightH, Breadth, opening, spaceH, notifyPoint) {
+    this.pairs = [
+        new pairHurdling(heightH, opening, Breadth),
+        new pairHurdling(heightH, opening, Breadth + spaceH),
+        new pairHurdling(heightH, opening, Breadth + spaceH * 2),
+        new pairHurdling(heightH, opening, Breadth + spaceH * 3)
+    ]
+    const displacement  = 3
+    this.animateH = () => {
+        this.pairs.forEach(pair => {
+            pair.setX(pair.getX() - displacement)
+
+            // when element get out game area
+            if  (pair.getX() < -pair.getBreadth()){
+                pair.setX(pair.getX() + spaceH * this.pairs.length)
+                pair.raffleOpening()
+            }
+            const middleH = Breadth / 2
+            const crossMiddle = pair.getX( ) + displacement >= middleH
+                && pair.getX( ) < middleH
+                if(crossMiddle)  notifyPoint( )
+        })
+    }
+}
+
+const hurdlingsH = new hurdlings(700, 1200, 200, 400)
+const areaGame =  document.querySelector('[wm-flappy]')
+hurdlingsH.pairs.forEach(pair => areaGame.appendChild(pair.element))
+setInterval( () => {
+    hurdlingsH.animateH( )
+}, 20)
